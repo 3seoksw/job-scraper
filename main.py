46f8e3f8e9c6a2b7552c49a2dd1c0e215648220a
3seoksw <3suksw@gmail.com>
2023-01-04 20:50:00 +0900

    Using Selenium to prevent anti-bot browser

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-from requests import get
+from selenium import webdriver
+from selenium.webdriver.safari.options import Options as SafariOptions
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "react"
+options = SafariOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-response = get(f"{base_url}{search_term}")
+browser = webdriver.Safari(options=options)
 
-if response.status_code != 200:
-    print("Unable to reach to the website")
-else:
-    results = []
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_ = "jobs")
-
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1) # deletes "view all" class
-
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1] # just extract required a tag 
-
-            link = anchor['href']
-
-            company, kind, region = anchor.find_all('span', class_ = "company")
-
-            title = anchor.find('span', class_ = "title")
-
-            job_data = {
-                'company': company.string,
-                'region': region.string,
-                'position': title.string
-            }
-            results.append(job_data)
-
-    for result in results:
-        print(result)
-        print("-------------------------------------")
+browser.get("https://www.indeed.com/jobs?q=python&limit=50")
