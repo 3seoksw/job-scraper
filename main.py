2a96f43b709e6006995685ec8c94af2d0dd5b953
3seoksw <3suksw@gmail.com>
2023-01-06 16:43:00 +0900

    Final_06JAN22

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("Job Scraper")
 
+db = {} # cache data
+
 @app.route("/")
 def home():
-    return render_template("home.html", name = "WS K")
+    return render_template("home.html")
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+
+    if keyword == None:
+        return redirect("/")
+
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+
+    return render_template("search.html", keyword = keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    
+    if keyword == None:
+        return redirect("/")
+
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run("127.0.0.1")
